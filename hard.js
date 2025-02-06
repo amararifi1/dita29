457e656f101091fc9208494473be8724fb428f36
Milaim Hajni (Zeka) <amararifi1234@gmail.com>
2025-02-06 19:16:17 +0100

    Add files via upload

---
+++ hard.js
@@ -0,0 +1,5 @@
+var hard = ["first","second","third","fourth","fifth","sixth","seventh","eighth"]
+
+var randomIndex = Math.floor(Math.random()*hard.length)
+var [randomnum] = [hard[randomIndex]]
+console.log(randomnum)
